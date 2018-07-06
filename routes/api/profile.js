371bcd81e36e9dac97ff7731cc1617e3d7ca8d32
kirill <kirill121@yahoo.com>
2018-07-06 13:20:01 +0200

    'update1_0'

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput = require('../../validation/education');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -126,6 +128,89 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 				})
 			}
 		})
-})
+});
+
+router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const { errors, isValid } = validateExperienceInput(req.body);
+
+	if(!isValid) {
+		return res.status(400).json(errors);
+	}
+
+	Profile.findOne({ user: req.user.id })
+		.then(profile => {
+			const newExp = {
+				title: req.body.title,
+				company: req.body.company,
+				location: req.body.location,
+				from: req.body.from,
+				to: req.body.to,
+				current: req.body.current,
+				description: req.body.description
+			}
+
+			profile.experience.unshift(newExp);
+
+			profile.save().then(profile => res.json(profile));	
+		})
+});
+
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const { errors, isValid } = validateEducationInput(req.body);
+
+	if(!isValid) {
+		return res.status(400).json(errors);
+	}
+
+	Profile.findOne({ user: req.user.id })
+		.then(profile => {
+			const newEdu = {
+				school: req.body.school,
+				degree: req.body.degree,
+				fieldofstudy: req.body.fieldofstudy,
+				from: req.body.from,
+				to: req.body.to,
+				current: req.body.current,
+				description: req.body.description
+			}
+
+			profile.education.unshift(newEdu);
+
+			profile.save().then(profile => res.json(profile));	
+		})
+});
+
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Profile.findOne({ user: req.user.id })
+		.then(profile => {
+			const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+			profile.experience.splice(removeIndex, 1)
+
+			profile.save().then(profile => res.json(profile));	
+		})
+		.catch(err => res.status(404).json(err))
+});
+
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Profile.findOne({ user: req.user.id })
+		.then(profile => {
+			const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+			profile.education.splice(removeIndex, 1)
+
+			profile.save().then(profile => res.json(profile));	
+		})
+		.catch(err => res.status(404).json(err))
+});
+
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Profile.findOneAndRemove({ user: req.user.id })
+		.then(() => {
+			User.findOneAndRemove({ _id: req.user.id })
+				.then(() => res.json({ success: true }))
+		})
+});
+
 
 module.exports = router;
